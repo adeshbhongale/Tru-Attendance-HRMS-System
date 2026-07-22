d8ac67419b07fd343ac7229f19c855c537dfdde4
Adesh Bhongale <adeshbhongale03@gmail.com>
2026-07-22 12:00:39 +0530

    fix: i update the some changes in mobile app.

--- mobile-app/app.config.js
+++ mobile-app/app.config.js
@@ -8,8 +8,16 @@ module.exports = ({ config }) => {
         locationAlwaysAndWhenInUsePermission: "Allow Geo-Track HRMS to access your location for attendance tracking.",
         isAndroidBackgroundLocationEnabled: true,
         isAndroidForegroundServiceEnabled: true
+      }],
+      ["react-native-maps", {
+        "androidGoogleMapsApiKey": process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY || "AIzaSyCAvI2O3xVUJrpARkgMTaH9_nOu1pif80Y"
       }]
     ],
+    updates: {
+      enabled: false,
+      checkOnLaunch: "NEVER",
+      fallbackToCacheTimeout: 0
+    },
     android: {
       ...config.android,
       config: {
